ac95ed0c6ce660806668cd1eb3d0acf8e32fb416
landoria-automation <landoria-automation@users.noreply.github.com>
2026-08-27 13:12:03 +0000

    Release HammerFreedom 1.0.6 [skip mod test]

--- Landoria.HammerFreedom/Properties/AssemblyInfo.cs
+++ Landoria.HammerFreedom/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.6.*")]
 [assembly: AssemblyFileVersion("1.0.6")]
 
-[assembly: AssemblyInformationalVersion("1.0.6-draft")]
+[assembly: AssemblyInformationalVersion("1.0.6")]
