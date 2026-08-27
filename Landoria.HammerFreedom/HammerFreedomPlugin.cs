656239ad5c6f4733a57b84771a096b4426b42cd5
landoria-automation <landoria-automation@users.noreply.github.com>
2026-08-27 10:13:28 +0000

    Create HammerFreedom 1.0.6-draft

--- Landoria.HammerFreedom/HammerFreedomPlugin.cs
+++ Landoria.HammerFreedom/HammerFreedomPlugin.cs
@@ -9,7 +9,7 @@ namespace Landoria.HammerFreedom
     {
         internal const string PluginGuid = "Landoria.HammerFreedom";
         internal const string PluginName = "Landoria.HammerFreedom";
-        internal const string PluginVersion = "1.0.5";
+        internal const string PluginVersion = "1.0.6";
         private static readonly KeyboardShortcut ToggleShortcut =
             new KeyboardShortcut(UnityEngine.KeyCode.Z);
 
