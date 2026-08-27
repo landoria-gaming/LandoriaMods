64b9a12aa1fdfdf0274b531cc999f208b08b8769
landoria-automation <landoria-automation@users.noreply.github.com>
2026-08-27 13:20:32 +0000

    Release StructureProtection 1.0.2 [skip mod test]

--- Landoria.StructureProtection/Properties/AssemblyInfo.cs
+++ Landoria.StructureProtection/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.2.*")]
 [assembly: AssemblyFileVersion("1.0.2")]
 
-[assembly: AssemblyInformationalVersion("1.0.2-draft")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
