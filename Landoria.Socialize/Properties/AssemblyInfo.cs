2f6d04cb96742746027c9bcfaa71a74c6de7691b
landoria-automation <landoria-automation@users.noreply.github.com>
2026-08-29 08:32:47 +0000

    Release Socialize 1.0.13 [skip mod test]

--- Landoria.Socialize/Properties/AssemblyInfo.cs
+++ Landoria.Socialize/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.13.*")]
 [assembly: AssemblyFileVersion("1.0.13")]
 
-[assembly: AssemblyInformationalVersion("1.0.13-draft")]
+[assembly: AssemblyInformationalVersion("1.0.13")]
