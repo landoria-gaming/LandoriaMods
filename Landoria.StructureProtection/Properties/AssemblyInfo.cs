ce8c151c9e03859500afad1fc8887b5d1b96ed6b
landoria-automation <landoria-automation@users.noreply.github.com>
2026-09-03 09:16:45 +0000

    Create StructureProtection 1.0.4-draft

--- Landoria.StructureProtection/Properties/AssemblyInfo.cs
+++ Landoria.StructureProtection/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("C6FAAFDF-2D25-4CD3-BD71-59BAA98F1D20")]
-[assembly: AssemblyVersion("1.0.3.*")]
-[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.4.*")]
+[assembly: AssemblyFileVersion("1.0.4")]
 
-[assembly: AssemblyInformationalVersion("1.0.3")]
+[assembly: AssemblyInformationalVersion("1.0.4-draft")]
--- Landoria.StructureProtection/StructureProtectionPlugin.cs
+++ Landoria.StructureProtection/StructureProtectionPlugin.cs
@@ -9,7 +9,7 @@ namespace Landoria.StructureProtection
     {
         private const string PluginGuid = "Landoria.StructureProtection";
         private const string PluginName = "Landoria.StructureProtection";
-        private const string PluginVersion = "1.0.3";
+        private const string PluginVersion = "1.0.4";
 
         internal static ModLog Log { get; private set; }
         internal static StructureProtectionSettings Settings { get; private set; }
