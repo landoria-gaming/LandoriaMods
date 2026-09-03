c012db291fe635fea45cf3c697c314698aa31f40
landoria-automation <landoria-automation@users.noreply.github.com>
2026-09-03 08:40:00 +0000

    Create DecayControl 1.0.3-draft

--- Landoria.DecayControl/DecayControlPlugin.cs
+++ Landoria.DecayControl/DecayControlPlugin.cs
@@ -8,7 +8,7 @@ namespace Landoria.DecayControl
     {
         private const string PluginGuid = "Landoria.DecayControl";
         private const string PluginName = "Landoria.DecayControl";
-        private const string PluginVersion = "1.0.2";
+        private const string PluginVersion = "1.0.3";
 
         internal static ModLog Log { get; private set; }
         internal static DecayControlSettings Settings { get; private set; }
