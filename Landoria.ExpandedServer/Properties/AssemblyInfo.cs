f1d52fd14a4b1294ba8d46ff33e8b3f6699301ca
landoria-automation <landoria-automation@users.noreply.github.com>
2026-09-04 19:48:54 +0000

    Release packages: ModSentry CharacterVault Socialize AfkDetector ExpandedServer [skip mod test]

--- Landoria.ExpandedServer/Properties/AssemblyInfo.cs
+++ Landoria.ExpandedServer/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.12.*")]
 [assembly: AssemblyFileVersion("1.0.12")]
-[assembly: AssemblyInformationalVersion("1.0.12-draft")]
+[assembly: AssemblyInformationalVersion("1.0.12")]
--- Landoria.Socialize/Properties/AssemblyInfo.cs
+++ Landoria.Socialize/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.15.*")]
 [assembly: AssemblyFileVersion("1.0.15")]
 
-[assembly: AssemblyInformationalVersion("1.0.15-draft")]
+[assembly: AssemblyInformationalVersion("1.0.15")]
