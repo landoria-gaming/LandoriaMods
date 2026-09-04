3f3417981394dab9d5552156f3695e07a2a20214
landoria-automation <landoria-automation@users.noreply.github.com>
2026-09-04 16:58:05 +0000

    Create mod drafts: ModSentry

--- Landoria.ModSentry/ModSentryPlugin.cs
+++ Landoria.ModSentry/ModSentryPlugin.cs
@@ -21,7 +21,7 @@ namespace Landoria.ModSentry
             UnverifiedGuestControllerRegistry.ProtocolVersion;
         private const string PluginGuid = "Landoria.ModSentry";
         private const string PluginName = "Landoria.ModSentry";
-        private const string PluginVersion = "1.0.14";
+        private const string PluginVersion = "1.0.15";
 
         internal static ModLog Log { get; private set; }
         internal static PluginPolicy Policy { get; private set; }
