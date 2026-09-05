8edff77f5704d59b153d599a7a52fffd1ffefbad
landoria-automation <landoria-automation@users.noreply.github.com>
2026-09-05 09:08:25 +0000

    Release packages: SealedTombstone [skip mod test]

--- Landoria.SealedTombstone/Properties/AssemblyInfo.cs
+++ Landoria.SealedTombstone/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.9.*")]
 [assembly: AssemblyFileVersion("1.0.9")]
 
-[assembly: AssemblyInformationalVersion("1.0.9-draft")]
+[assembly: AssemblyInformationalVersion("1.0.9")]
