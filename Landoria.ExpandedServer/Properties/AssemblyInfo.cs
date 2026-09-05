6473e25a6649c5867d3899a3f310940c149963fb
landoria-automation <landoria-automation@users.noreply.github.com>
2026-09-05 09:03:03 +0000

    Release packages: ExpandedServer Moderator FirstPerson GentleDeath NoServerPassword AfkDetector FreeFlyCommand GetMyTrophyBack QuickLaunch HammerFreedom [skip mod test]

--- Landoria.ExpandedServer/Properties/AssemblyInfo.cs
+++ Landoria.ExpandedServer/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.13.*")]
 [assembly: AssemblyFileVersion("1.0.13")]
-[assembly: AssemblyInformationalVersion("1.0.13-draft")]
+[assembly: AssemblyInformationalVersion("1.0.13")]
--- Landoria.NoServerPassword/Properties/AssemblyInfo.cs
+++ Landoria.NoServerPassword/Properties/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("C424AE43-8596-4E8C-9517-0B4BF2927364")]
 [assembly: AssemblyVersion("1.0.8.*")]
 [assembly: AssemblyFileVersion("1.0.8")]
-[assembly: AssemblyInformationalVersion("1.0.8-draft")]
+[assembly: AssemblyInformationalVersion("1.0.8")]
