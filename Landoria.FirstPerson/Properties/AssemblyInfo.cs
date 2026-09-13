57992f8174b189db74accf98839debfac7754fd5
landoria-automation <landoria-automation@users.noreply.github.com>
2026-09-13 09:33:41 +0000

    Release packages: FirstPerson [skip mod test]

--- Landoria.FirstPerson/Properties/AssemblyInfo.cs
+++ Landoria.FirstPerson/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.9.*")]
 [assembly: AssemblyFileVersion("1.0.9")]
 
-[assembly: AssemblyInformationalVersion("1.0.9-draft")]
+[assembly: AssemblyInformationalVersion("1.0.9")]
